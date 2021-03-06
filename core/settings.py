65db0de8af12bc6198ed163c205c6c2f30bc792f
saloniig <saloniggoyal@gmail.com>
2021-03-07 02:01:37 +0530

    touchup

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = False
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cust0mertracker.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
