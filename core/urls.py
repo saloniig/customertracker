0894bbdc3d91729a101f5853eee273142882e933
saloniig <saloniggoyal@gmail.com>
2021-10-11 21:59:37 +0530

    Updates code with minor fixes
    - Updates urls.py
    - Updates README.md

--- core/urls.py
+++ core/urls.py
@@ -7,9 +7,9 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    path("", views.index), 
+    path("", views.index),
     path("charts/", views.chart),
-   #path("visitors/", include("visitors.urls")),             # UI Kits Html files
+    # path("visitors/", include("visitors.urls")),             # UI Kits Html files
     url(r'^visitor/', include('visitors.urls')),
 
     path('admin/', admin.site.urls),
@@ -17,8 +17,9 @@ urlpatterns = [
     path('add', views.addsale),
     path('tables/', views.table),
     #path('', include('django.contrib.auth.urls')),
-    path("auth/", include("authentication.urls")), # Auth routes - login / register
-    #path("", include("app.urls"))             # UI Kits Html files
+    # Auth routes - login / register
+    path("auth/", include("authentication.urls")),
+    # path("", include("app.urls"))             # UI Kits Html files
 
 
 ]
